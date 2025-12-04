c5a4f5a9efa71e47ce21dec71e9943836cf7c399
Wyliam <wyliam.burger@epitech.eu>
2025-12-04 13:49:41 +0100

    AoC Day04

---
+++ 2025/day04/part01.py
@@ -0,0 +1,29 @@
+with open("input", "r") as file:
+    grid = [line.strip() for line in file]
+
+rows = len(grid)
+columns = len(grid[0])
+
+print(rows)
+print(columns)
+
+directions = [(-1, 1), (0, 1), (1, 1), (-1, 0), (1, 0), (-1, -1), (0, -1), (1, -1)]
+
+accessibleRolls = 0
+
+for row in range(rows):
+    for col in range(columns):
+        if grid[row][col] == "@":
+            counted = 0
+            for dr, dc in directions:
+                r_neighbor = row + dr
+                c_neighbor = col + dc
+
+                if 0 <= r_neighbor < rows and 0 <= c_neighbor < columns:
+                    if grid[r_neighbor][c_neighbor] == "@":
+                        counted += 1
+
+            if counted < 4:
+                accessibleRolls += 1
+
+print(accessibleRolls)
---
+++ 2025/day04/part02.py
@@ -0,0 +1,41 @@
+with open("input", "r") as file:
+    grid = [list(line.strip()) for line in file]
+
+rows = len(grid)
+columns = len(grid[0])
+directions = [(-1, 1), (0, 1), (1, 1), (-1, 0), (1, 0), (-1, -1), (0, -1), (1, -1)]
+
+total_removed = 0
+removed_in_last_round = 1
+
+while removed_in_last_round > 0:
+    removed_in_last_round = 0
+    rolls_to_remove = []
+
+    for r in range(rows):
+        for c in range(columns):
+            if grid[r][c] == "@":
+                neighbor_count = 0
+                for dr, dc in directions:
+                    nr = r + dr
+                    nc = c + dc
+
+                    if 0 <= nr < rows and 0 <= nc < columns:
+                        if grid[nr][nc] == "@":
+                            neighbor_count += 1
+
+                if neighbor_count < 4:
+                    rolls_to_remove.append((r, c))
+
+    removed_in_last_round = len(rolls_to_remove)
+
+    if removed_in_last_round > 0:
+        for r_remove, c_remove in rolls_to_remove:
+            grid[r_remove][c_remove] = "x"
+
+        total_removed += removed_in_last_round
+
+for row_list in grid:
+    print("".join(row_list))
+
+print(total_removed)
